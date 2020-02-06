f54d86efa384be9d478071d84ef7909727a4039e
Hgunn97 <37982876+Hgunn97@users.noreply.github.com>
2020-02-06 15:46:29 +0000

    Pretty sure it works

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import Home from './Components/Home'
-import './main.css';
-import Form from './Components/Form'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-  render(){
-    return (
-      <BrowserRouter>
-          <Switch>
-            <Home path='/' Component={Home} exact />
-            <Form path='/form' Component={Form} />
-          </Switch>
-      </BrowserRouter>
-    );
-  }
+import CreateUser from "./Components/create-user.component";
+import Users from "./Components/users.component";
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand">React Axios Tutorial</a>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to={"/create-user"}>Create User</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/users"}>Users List</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Switch>
+              <Route exact path='/' component={CreateUser} />
+              <Route path="/create-user" component={CreateUser} />
+              <Route path="/users" component={Users} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Router>
+  );
 }
 
 export default App;
---
+++ src/Components/data-table.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class DataTable extends React.Component{
+    render(){
+        return(
+            <tr>
+                <td>
+                    {this.props.obj._id}
+                </td>
+                <td>
+                    {this.props.obj.name}
+                </td>                
+                <td>
+                    {this.props.obj.email}
+                </td>
+            </tr>
+        )
+    }
+}
+export default DataTable
