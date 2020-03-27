f25fdbac94a8aee08493b8dedbd450c2f40df65a
Hannah Gunn <hannahgunn97@gmail.com>
2020-03-27 16:41:39 +0000

    Fully working
    - Can post new bookings
    - Get all bookings
    - Get individual bookings
    - Connected to MongoDB atlas

--- app.js
+++ app.js
@@ -1,60 +1,75 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var hbs = require('express-handlebars');
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose")
+const {mongoURL} = require('./keys')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
 
-var routes = require('./routes/index');
+const port = 4000;
 
-var app = express();
-
-// view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-// error handlers
+const postSchema = {
+    title: String,
+    description: String,
+    date: Date,
+    author: String
+};
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+const Post = mongoose.model("Bookings", postSchema);
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+        res.render('home')
+})
+
+app.post('/compose', (req, res) => {
+    const post = new Post({
+        title: req.body.postTitle,
+        description: req.body.postDescription,
+        date: req.body.postDate,
+        author: req.body.postAuthor
+    })
+
+    post.save(function (err) {
+        if(!err){
+            res.redirect("/")
+        }
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
+app.get('/posts', (req, res) => {
+    Post.find({}, (err, posts) => {
+        res.render('Posts', {
+            posts: posts
+        })
+    })
+})
 
-module.exports = app;
+app.get('/posts/:postId', (req, res) => {
+    const requestedPostId = req.params.postId;
+    Post.findOne({ _id: requestedPostId }, (err, post) => {
+        res.render("post", {
+            title: post.title,
+            description: post.description,
+            date: post.date,
+            author: post.author
+        });
+    });
+});
+
+app.get("/compose", (req, res) => {
+    res.render("compose")
+})
+
+//Listen to server
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
